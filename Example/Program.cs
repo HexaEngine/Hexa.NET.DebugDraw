2152944479e23a3b904046f25880be032a50522f
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-08-26 11:26:35 +0200

    Updated example

--- Example/Program.cs
+++ Example/Program.cs
@@ -15,9 +15,12 @@
             using var window = Window.Create(WindowOptions.Default);
 
             // Declare some variables
-            DebugDrawRenderer renderer = null;
-            GL gl = null;
-            IInputContext inputContext = null;
+            DebugDrawRenderer renderer = null!;
+            GL gl = null!;
+            IInputContext inputContext = null!;
+            IMouse mouse = null!;
+            IKeyboard keyboard = null!;
+            Vector2 lastMousePos = default;
 
             // Our loading function
             window.Load += () =>
@@ -27,6 +30,9 @@
                     window
                 );
                 inputContext = window.CreateInput();
+                mouse = inputContext.Mice[0];
+                keyboard = inputContext.Keyboards[0];
+                lastMousePos = mouse.Position;
             };
 
             // Handle resizes
@@ -38,12 +44,13 @@
             };
 
             Vector3 position = new(-5, 5, -5);
-            Quaternion rotation = new Vector3(45, 45, 0).ToRad().ToQuaternion();
+            Vector3 rotation = new(45, 45, 0);
+            Quaternion orientation = rotation.ToRad().ToQuaternion();
             Matrix4x4 projection = MathUtil.PerspectiveFovLH(
-                100f.ToRad(), window.Size.X / window.Size.Y, 0.1f, 1000f
+                90f.ToRad(), window.Size.X / window.Size.Y, 0.1f, 1000f
             );
-            Vector3 target = Vector3.Transform(Vector3.UnitZ, rotation);
-            Vector3 up = Vector3.Transform(Vector3.UnitY, rotation);
+            Vector3 target = Vector3.Transform(Vector3.UnitZ, orientation);
+            Vector3 up = Vector3.Transform(Vector3.UnitY, orientation);
             Matrix4x4 view = MathUtil.LookAtLH(position, position + target, up);
 
             Matrix4x4 vp = view * projection;
@@ -51,13 +58,58 @@
             // The render function
             window.Render += delta =>
             {
-                // Make sure ImGui is up-to-date
+                var now = mouse.Position;
+                var mouseDelta = now - lastMousePos;
+                lastMousePos = now;
+
+                if (mouseDelta != Vector2.Zero && mouse.IsButtonPressed(MouseButton.Left))
+                {
+                    rotation += new Vector3(mouseDelta * 0.004f * 20, 0);
+                    Update(position, rotation, out orientation, projection, out vp);
+                }
+
+                Vector3 direction = default;
+                if (keyboard.IsKeyPressed(Key.W))
+                {
+                    direction += Vector3.UnitZ;
+                }
+                if (keyboard.IsKeyPressed(Key.A))
+                {
+                    direction -= Vector3.UnitX;
+                }
+                if (keyboard.IsKeyPressed(Key.D))
+                {
+                    direction += Vector3.UnitX;
+                }
+                if (keyboard.IsKeyPressed(Key.S))
+                {
+                    direction -= Vector3.UnitZ;
+                }
+                if (keyboard.IsKeyPressed(Key.Space))
+                {
+                    direction += Vector3.UnitY;
+                }
+                if (keyboard.IsKeyPressed(Key.C))
+                {
+                    direction -= Vector3.UnitY;
+                }
+
+                if (direction != Vector3.Zero)
+                {
+                    direction *= 2;
+                    if (keyboard.IsKeyPressed(Key.ShiftLeft))
+                    {
+                        direction *= 2;
+                    }
+                    direction = Vector3.Transform(direction, orientation);
+                    position += direction * (float)delta;
+                    Update(position, rotation, out orientation, projection, out vp);
+                }
+
                 renderer.BeginDraw();
 
                 DebugDraw.SetCamera(vp);
 
-                // This is where you'll do any rendering beneath the ImGui context
-                // Here, we just have a blank screen.
                 gl.ClearColor(System.Drawing.Color.FromArgb(255, (int)(.45f * 255), (int)(.55f * 255), (int)(.60f * 255)));
                 gl.Clear((uint)ClearBufferMask.ColorBufferBit);
                 gl.ClearDepth(1.0f);
@@ -79,7 +131,6 @@
             // The closing function
             window.Closing += () =>
             {
-                // Dispose our controller first
                 renderer?.Dispose();
 
                 // Dispose the input context
@@ -93,6 +144,15 @@
             window.Run();
 
             window.Dispose();
+        }
+
+        private static void Update(Vector3 position, Vector3 rotation, out Quaternion orientation, Matrix4x4 projection, out Matrix4x4 vp)
+        {
+            orientation = rotation.ToRad().ToQuaternion();
+            Vector3 target = Vector3.Transform(Vector3.UnitZ, orientation);
+            Vector3 up = Vector3.Transform(Vector3.UnitY, orientation);
+            Matrix4x4 view = MathUtil.LookAtLH(position, position + target, up);
+            vp = view * projection;
         }
     }
 }
